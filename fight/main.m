e4ad76dd3587c586919ce9ca6a96bc6551f6918b
yodacode <benjamin.devaublanc@gmail.com>
2013-06-18 14:57:28 +0200

    Initial Commit

---
+++ fight/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  fight
+//
+//  Created by Benjamin on 18/06/13.
+//  Copyright (c) 2013 Benjamin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
